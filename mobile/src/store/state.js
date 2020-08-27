9d38886b48d18a271114c8bed86f7fed274f02ab
小辉 <1652016835@qq.com>
2020-08-27 16:40:32 +0800

    mobile'

---
+++ mobile/src/store/state.js
@@ -0,0 +1,17 @@
+//唯一的数据源
+export default {
+    todos:[
+        {
+            task:'吃饭',
+            done:true
+        },
+        {
+            task:'睡觉',
+            done:false
+        },
+        {
+            task:'打游戏',
+            done:false
+        },                             
+    ]
+}
