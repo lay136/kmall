4b4753fcc2057370a38636f709711924a82b746f
lay136 <1652016835@qq.com>
2020-08-31 00:33:10 +0800

    'article'

--- miniprogram/pages/article/article.js
+++ miniprogram/pages/article/article.js
@@ -5,6 +5,28 @@ Page({
      * 页面的初始数据
      */
     data: {
+        articles:[
+            {
+                author:'小辉',
+                avatar:'/images/avatar/u1.jpg',
+                date:'发表于：今天',
+                title:'返校第一天！',
+                img:'/images/article/a1.jpg',
+                desc:'阔别八个月，我们将再次于许院重聚，再次见到亲爱的老师和同学，再次回到亲切的校园，在见到的那一刻，都化为了心中的汩汩暖流。',
+                star:30,
+                view:20
+            },
+            {
+                author:'小明',
+                avatar: '/images/avatar/u2.jpg',
+                date: '2020-08-30',
+                title: '许院，我们回来了！',
+                img: '/images/article/a2.jpg',
+                desc: '阔别稍久，终如期相逢！8月28—30日，我校迎来了17级、18级、19级学生秋季学期集中返校报到。',
+                star: 30,
+                view: 20
+            },      
+          ]
 
     },
 
